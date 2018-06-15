762929f29573a5dfc4afe78dda97ed33f678bc69
Nadav Kaner <nadavkaner1@gmail.com>
2018-06-15 09:10:25 +0300

    add electron

---
+++ index.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
